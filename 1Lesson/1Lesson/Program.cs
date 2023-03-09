bac284c148a0b59f06807eed17c4e0e865c71c79
PolinaSmolina <polina.smolina.00@bk.ru>
2023-03-09 22:56:01 +0700

    Well done Make project for HW: 1Lesson

---
+++ 1Lesson/1Lesson/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Write here your name, please");
+        string name = Console.ReadLine();
+        Console.WriteLine(name + ", Hello!!!");
+    }
+}
