c0badfe545b7734cc80b7e30469cced04fa6c40d
PolinaSmolina <polina.smolina.00@bk.ru>
2023-03-24 16:57:29 +0700

    make 2 HW

---
+++ 2HomeWork/2HomeWork/Program.cs
@@ -0,0 +1,43 @@
+﻿
+public class Program
+{ 
+public static void Main()
+{
+    Console.WriteLine("Введи колличество элементов массива:");
+    string text = Console.ReadLine();
+    int countElements;
+    int[] thisArray;
+    try
+    {
+        countElements = Convert.ToInt32(text);
+        thisArray = new int[countElements];
+        for (int i = 0; i < thisArray.Length; i++)
+        {
+            Console.WriteLine($"Введи элемент под индексом {i}");
+            thisArray[i] = int.Parse(Console.ReadLine());
+        }
+        int[] newThisArray = thisArray.Distinct().ToArray(); //удаление дублей
+        /*for (int i = 0; i < newThisArray.Length; i++) //проверка вывода без дублей
+        {
+            Console.WriteLine(newThisArray[i]);
+        }
+        */
+        Array.Sort(newThisArray);
+        Console.WriteLine($"Твоё число: {newThisArray[newThisArray.Length - 2]}");
+
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Вводи только целые числа");
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("В твоём массиве меньше двух элементов,или число повторяющихся элементов равно числу всех элементов в массиве. Исправь!");
+    }
+    finally
+    {
+        Console.WriteLine("Программа сработала!");
+    }
+}
+
+}
