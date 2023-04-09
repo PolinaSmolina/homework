85c4a825d085cac87a8ba1f9c5c03bc5f9ac0349
PolinaSmolina <polina.smolina.00@bk.ru>
2023-04-09 21:13:16 +0700

    make 4HW

---
+++ 4Lesson/4Lesson/Handler1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    public class Handler1
+    {
+        public void DisplayMessage() 
+        {
+            Console.WriteLine("Пора действовать, уже ведь 77");
+        }
+
+    }
+}
